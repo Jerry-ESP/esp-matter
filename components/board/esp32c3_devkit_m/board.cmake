aa8dfb46b5c7d5f8816ad8a52b7b45e19cf53b67
Shu Chen <chenshu@espressif.com>
2021-07-16 14:28:52 +0800

    config BOARD_NAME in Kconfig

--- components/board/esp32c3_devkit_m/board.cmake
+++
@@ -1,7 +0,0 @@
-# Designed to be included from the app's CMakeLists.txt file
-cmake_minimum_required(VERSION 3.5)
-
-# These paths are used in other components
-set(BOARD_PATH ${BOARD_NAME} CACHE INTERNAL "")
-set(LED_DRIVER_PATH "rmt" CACHE INTERNAL "")
-set(BUTTON_DRIVER_PATH "hollow" CACHE INTERNAL "")
