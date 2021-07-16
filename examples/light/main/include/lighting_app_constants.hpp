6f01f2ecdd95e3229df8a362d3a93abd5ef2e41b
Chirag Atal <chirag.atal@espressif.com>
2021-07-16 19:55:33 +0530

    rename: Renaming examples to light and rainmaker_light

--- examples/light/main/include/lighting_app_constants.hpp
+++ examples/light/main/include/lighting_app_constants.hpp
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License
 
-#define APP_LOG_TAG "lighting-app"
+#define APP_LOG_TAG "light"
 
 #define DEFAULT_POWER true
 #define DEFAULT_BRIGHTNESS 100
