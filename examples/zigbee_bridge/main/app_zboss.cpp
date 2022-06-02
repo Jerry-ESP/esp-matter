8db60e7ce14d7c62afc70cbe17913960d7ddc0cf
WanqQixiang <wangqixiang@espressif.com>
2022-06-02 11:36:40 +0800

    Receive report from lvmi door sensor

--- examples/zigbee_bridge/main/app_zboss.cpp
+++ examples/zigbee_bridge/main/app_zboss.cpp
@@ -13,10 +13,31 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <zboss_api.h>
+#include "ha/zb_ha_configuration_tool.h"
 
 #if (!defined ZB_MACSPLIT_HOST)
 #error "Zigbee host option should be enabled to use this example"
 #endif
+
+#define ZB_DEVICE_ENDPOINT 1
+
+/* Basic cluster attributes data */
+zb_uint8_t g_attr_zcl_version  = ZB_ZCL_VERSION;
+zb_uint8_t g_attr_power_source = ZB_ZCL_BASIC_POWER_SOURCE_UNKNOWN;
+
+ZB_ZCL_DECLARE_BASIC_ATTRIB_LIST(basic_attr_list, &g_attr_zcl_version, &g_attr_power_source);
+
+/* Identify cluster attributes data */
+zb_uint16_t g_attr_identify_time = ZB_ZCL_IDENTIFY_IDENTIFY_TIME_DEFAULT_VALUE;
+
+ZB_ZCL_DECLARE_IDENTIFY_ATTRIB_LIST(identify_attr_list, &g_attr_identify_time);
+
+ZB_HA_DECLARE_CONFIGURATION_TOOL_CLUSTER_LIST(bridge_clusters,
+                                 basic_attr_list, identify_attr_list);
+
+ZB_HA_DECLARE_CONFIGURATION_TOOL_EP(bridge_ep, ZB_DEVICE_ENDPOINT, bridge_clusters);
+
+ZB_HA_DECLARE_CONFIGURATION_TOOL_CTX(bridge_ctx, bridge_ep);
 
 static const char *TAG = "esp_zboss";
 
@@ -25,6 +46,45 @@ static void bdb_start_top_level_commissioning_cb(zb_uint8_t mode_mask)
     if (!bdb_start_top_level_commissioning(mode_mask)) {
         ESP_LOGE(TAG, "In BDB commissioning, an error occurred (for example: the device has already been running)");
     }
+}
+
+void zcl_process_attribute_reporting(zb_uint8_t param)
+{
+    zb_zcl_parsed_hdr_t *cmd_info = ZB_BUF_GET_PARAM(param, zb_zcl_parsed_hdr_t);
+    zb_uint8_t src_ep = ZB_ZCL_PARSED_HDR_SHORT_DATA(cmd_info).src_endpoint;
+    zb_uint8_t dst_ep = ZB_ZCL_PARSED_HDR_SHORT_DATA(cmd_info).dst_endpoint;
+
+    zb_zcl_report_attr_req_t *rep_attr_req;
+
+    do
+    {
+        ZB_ZCL_GENERAL_GET_NEXT_REPORT_ATTR_REQ(param, rep_attr_req);
+        if (rep_attr_req)
+        {
+            ESP_LOGI(TAG, "Report command is received, src_ep %d, dst_ep %d, cluster_id 0x%x, attr_id 0x%x",
+                    src_ep, dst_ep, cmd_info->cluster_id, rep_attr_req->attr_id);
+            if (cmd_info->cluster_id == ZB_ZCL_CLUSTER_ID_ON_OFF &&
+                rep_attr_req->attr_id == ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID)
+            {
+                ESP_LOGI(TAG, "receive report command, send command to the matter device");
+                // send command to matter device.
+            }
+        }
+    } while (rep_attr_req);
+}
+
+zb_uint8_t zcl_custom_cmd_handler(zb_uint8_t param)
+{
+    zb_zcl_parsed_hdr_t *cmd_info = ZB_BUF_GET_PARAM(param, zb_zcl_parsed_hdr_t);
+    /* Store some values - cmd_info will be overwritten */
+    zb_uint8_t processed = ZB_FALSE;
+
+    if(cmd_info->is_common_command && cmd_info->cmd_id == ZB_ZCL_CMD_REPORT_ATTRIB)
+    {
+        zcl_process_attribute_reporting(param);
+    }
+
+  return processed;
 }
 
 /**
@@ -109,17 +169,19 @@ void zboss_signal_handler(zb_bufid_t bufid)
     }
 }
 
-void zboss_task()
+void zboss_task(void *pvParameters)
 {
     ZB_INIT("zigbee bridge");
     zb_set_network_coordinator_role(IEEE_CHANNEL_MASK);
     zb_set_nvram_erase_at_start(ERASE_PERSISTENT_CONFIG);
     zb_set_max_children(MAX_CHILDREN);
+
+    ZB_AF_REGISTER_DEVICE_CTX(&bridge_ctx);
+    ZB_AF_SET_ENDPOINT_HANDLER(ZB_DEVICE_ENDPOINT, zcl_custom_cmd_handler);
     /* initiate Zigbee Stack start without zb_send_no_autostart_signal auto-start */
     ESP_ERROR_CHECK(zboss_start_no_autostart());
     while (1) {
         zboss_main_loop_iteration();
-        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
 
