8a53acf61b383ff82dde742404d815fd7b997c76
Shubham Patil <shubham.patil@espressif.com>
2023-02-28 13:15:38 +0530

    buzzer example

---
+++ examples/buzzer/main/app_main.cpp
@@ -0,0 +1,99 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_err.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+
+#include <esp_matter.h>
+#include <esp_matter_ota.h>
+
+#include <app_priv.h>
+#include <app_reset.h>
+
+#include <app/server/CommissioningWindowManager.h>
+#include <app/server/Server.h>
+static const char *TAG = "app_main";
+uint16_t buzzer_endpoint_id = 0;
+
+using namespace esp_matter;
+using namespace esp_matter::attribute;
+using namespace esp_matter::endpoint;
+using namespace chip::app::Clusters;
+
+constexpr auto k_timeout_seconds = 300;
+
+static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
+{
+    switch (event->Type) {
+    case chip::DeviceLayer::DeviceEventType::kCommissioningComplete:
+        ESP_LOGI(TAG, "Commissioning complete");
+        break;
+
+    default:
+        break;
+    }
+}
+
+static esp_err_t app_identification_cb(identification::callback_type_t type, uint16_t endpoint_id, uint8_t effect_id,
+                                       void *priv_data)
+{
+    ESP_LOGI(TAG, "Identification callback: type: %d, effect: %d", type, effect_id);
+    return ESP_OK;
+}
+
+static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
+                                         uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data)
+{
+    esp_err_t err = ESP_OK;
+
+    if (type == PRE_UPDATE) {
+        /* Driver update */
+        app_driver_handle_t driver_handle = (app_driver_handle_t)priv_data;
+        err = app_driver_attribute_update(driver_handle, endpoint_id, cluster_id, attribute_id, val);
+    }
+
+    return err;
+}
+
+extern "C" void app_main()
+{
+    esp_err_t err = ESP_OK;
+
+    /* Initialize the ESP NVS layer */
+    nvs_flash_init();
+
+    /* Initialize driver */
+    app_driver_handle_t buzzer_handle = app_driver_buzzer_init();
+    app_driver_handle_t button_handle = app_driver_button_init();
+    app_reset_button_register(button_handle);
+
+    /* Create a Matter node and add the mandatory Root Node device type on endpoint 0 */
+    node::config_t node_config;
+    node_t *node = node::create(&node_config, app_attribute_update_cb, app_identification_cb);
+
+    on_off_plugin_unit::config_t buzzer_config;
+    endpoint_t *endpoint = on_off_plugin_unit::create(node, &buzzer_config, ENDPOINT_FLAG_NONE, buzzer_handle);
+
+    /* These node and endpoint handles can be used to create/add other endpoints and clusters. */
+    if (!node || !endpoint) {
+        ESP_LOGE(TAG, "Matter node creation failed");
+    }
+
+    buzzer_endpoint_id = endpoint::get_id(endpoint);
+    ESP_LOGI(TAG, "Light created with endpoint_id %d", buzzer_endpoint_id);
+
+    /* Matter start */
+    err = esp_matter::start(app_event_cb);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Matter start failed: %d", err);
+    }
+
+    /* Starting driver with default values */
+    app_driver_buzzer_set_defaults(buzzer_endpoint_id);
+}
