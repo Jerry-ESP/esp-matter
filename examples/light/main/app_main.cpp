3fc9b33efeb138013a3e0384af0ab17062aa7532
liyashuai <liyashuai@espressif.com>
2025-01-16 20:18:22 +0800

    update

--- examples/light/main/app_main.cpp
+++ examples/light/main/app_main.cpp
@@ -137,7 +137,7 @@ static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16
     if (type == PRE_UPDATE) {
         /* Driver update */
         app_driver_handle_t driver_handle = (app_driver_handle_t)priv_data;
-        err = app_driver_attribute_update(driver_handle, endpoint_id, cluster_id, attribute_id, val);
+        //err = app_driver_attribute_update(driver_handle, endpoint_id, cluster_id, attribute_id, val);
     }
 
     return err;
@@ -162,35 +162,49 @@ extern "C" void app_main()
     node_t *node = node::create(&node_config, app_attribute_update_cb, app_identification_cb);
     ABORT_APP_ON_FAILURE(node != nullptr, ESP_LOGE(TAG, "Failed to create Matter node"));
 
-    extended_color_light::config_t light_config;
-    light_config.on_off.on_off = DEFAULT_POWER;
-    light_config.on_off.lighting.start_up_on_off = nullptr;
-    light_config.level_control.current_level = DEFAULT_BRIGHTNESS;
-    light_config.level_control.on_level = DEFAULT_BRIGHTNESS;
-    light_config.level_control.lighting.start_up_current_level = DEFAULT_BRIGHTNESS;
-    light_config.color_control.color_mode = (uint8_t)ColorControl::ColorMode::kColorTemperature;
-    light_config.color_control.enhanced_color_mode = (uint8_t)ColorControl::ColorMode::kColorTemperature;
-    light_config.color_control.color_temperature.startup_color_temperature_mireds = nullptr;
+    on_off_light::config_t onoff_config;
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
+    on_off_light::create(node, &onoff_config, ENDPOINT_FLAG_NONE, nullptr);
 
-    // endpoint handles can be used to add/modify clusters.
-    endpoint_t *endpoint = extended_color_light::create(node, &light_config, ENDPOINT_FLAG_NONE, light_handle);
-    ABORT_APP_ON_FAILURE(endpoint != nullptr, ESP_LOGE(TAG, "Failed to create extended color light endpoint"));
+    // extended_color_light::config_t light_config;
+    // light_config.on_off.on_off = DEFAULT_POWER;
+    // light_config.on_off.lighting.start_up_on_off = nullptr;
+    // light_config.level_control.current_level = DEFAULT_BRIGHTNESS;
+    // light_config.level_control.on_level = DEFAULT_BRIGHTNESS;
+    // light_config.level_control.lighting.start_up_current_level = DEFAULT_BRIGHTNESS;
+    // light_config.color_control.color_mode = (uint8_t)ColorControl::ColorMode::kColorTemperature;
+    // light_config.color_control.enhanced_color_mode = (uint8_t)ColorControl::ColorMode::kColorTemperature;
+    // light_config.color_control.color_temperature.startup_color_temperature_mireds = nullptr;
 
-    light_endpoint_id = endpoint::get_id(endpoint);
-    ESP_LOGI(TAG, "Light created with endpoint_id %d", light_endpoint_id);
+    // // endpoint handles can be used to add/modify clusters.
+    // endpoint_t *endpoint = extended_color_light::create(node, &light_config, ENDPOINT_FLAG_NONE, light_handle);
+    // ABORT_APP_ON_FAILURE(endpoint != nullptr, ESP_LOGE(TAG, "Failed to create extended color light endpoint"));
 
-    /* Mark deferred persistence for some attributes that might be changed rapidly */
-    cluster_t *level_control_cluster = cluster::get(endpoint, LevelControl::Id);
-    attribute_t *current_level_attribute = attribute::get(level_control_cluster, LevelControl::Attributes::CurrentLevel::Id);
-    attribute::set_deferred_persistence(current_level_attribute);
+    // light_endpoint_id = endpoint::get_id(endpoint);
+    // ESP_LOGI(TAG, "Light created with endpoint_id %d", light_endpoint_id);
 
-    cluster_t *color_control_cluster = cluster::get(endpoint, ColorControl::Id);
-    attribute_t *current_x_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::CurrentX::Id);
-    attribute::set_deferred_persistence(current_x_attribute);
-    attribute_t *current_y_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::CurrentY::Id);
-    attribute::set_deferred_persistence(current_y_attribute);
-    attribute_t *color_temp_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::ColorTemperatureMireds::Id);
-    attribute::set_deferred_persistence(color_temp_attribute);
+    // /* Mark deferred persistence for some attributes that might be changed rapidly */
+    // cluster_t *level_control_cluster = cluster::get(endpoint, LevelControl::Id);
+    // attribute_t *current_level_attribute = attribute::get(level_control_cluster, LevelControl::Attributes::CurrentLevel::Id);
+    // attribute::set_deferred_persistence(current_level_attribute);
+
+    // cluster_t *color_control_cluster = cluster::get(endpoint, ColorControl::Id);
+    // attribute_t *current_x_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::CurrentX::Id);
+    // attribute::set_deferred_persistence(current_x_attribute);
+    // attribute_t *current_y_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::CurrentY::Id);
+    // attribute::set_deferred_persistence(current_y_attribute);
+    // attribute_t *color_temp_attribute = attribute::get(color_control_cluster, ColorControl::Attributes::ColorTemperatureMireds::Id);
+    // attribute::set_deferred_persistence(color_temp_attribute);
 
 #if CHIP_DEVICE_CONFIG_ENABLE_THREAD && CHIP_DEVICE_CONFIG_ENABLE_WIFI_STATION
     // Enable secondary network interface
